818b7aa5b14ab282e3a55125c536acbc970fe96f
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-01 15:35:14 +0700

    fi

--- src/app/terimakasih/page.js
+++ src/app/terimakasih/page.js
@@ -1,11 +1,11 @@
 'use client'
 import Link from "next/link"
 import { redirect } from "next/navigation"
-
+P
 
 export default function Terimakasih() {
-  if(typeof window !== undefined) {
-     const session = sessionStorage.getItem('data')
+  if(typeof window !== "undefined") {
+     const session = sessionStorage.getItem('data') 
     if(!session) {
       redirect('/')
     } else {
@@ -21,7 +21,7 @@ export default function Terimakasih() {
     <div className="hero min-h-screen bg-[#ffa500]">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold font-bold text-3xl uppercase">Terima Kasih</h1>
+      <h1 className=" font-bold text-3xl uppercase">Terima Kasih</h1>
       <p className="py-6">pesanan sedang diproses yaa.. selamat menikmati seblak kami.. salam pedassss 🔥🔥🔥</p>
      <Link href={'/'}> <button className="btn btn-primary bg-orange-600 border-none hover:bg-orange-400 ">Beranda</button></Link>
      <Link href={'/pages/list_menu'}> <button className="btn btn-primary bg-orange-600 border-none hover:bg-orange-400">Beli Lagi</button></Link>
